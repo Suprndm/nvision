78ea02c55755b21c5fa896879b258d29279a2a29
Keitaro92 <nicolas.dalmolin@gmail.com>
2017-05-08 17:19:57 +0200

    add webApi

---
+++ NVision.WebApi/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace NVision.WebApi.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("health")]
+        public IHttpActionResult Health()
+        {
+
+            return Ok("I'm alive !");
+        }
+    }
+}
---
+++ NVision.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NVision.WebApi.Global" Language="C#" %>
